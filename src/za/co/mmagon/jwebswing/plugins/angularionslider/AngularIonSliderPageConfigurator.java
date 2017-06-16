3c983ae83110896b56753bb48a9a0cb3e4de3e5e
Marc Magon <ged_marc@hotmail.com>
2017-06-16 19:09:36 +0200

    first commit

---
+++ src/za/co/mmagon/jwebswing/plugins/angularionslider/AngularIonSliderPageConfigurator.java
@@ -0,0 +1,57 @@
+package za.co.mmagon.jwebswing.plugins.angularionslider;
+
+import za.co.mmagon.jwebswing.Page;
+import za.co.mmagon.jwebswing.PageConfigurator;
+import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
+import za.co.mmagon.jwebswing.plugins.PluginInformation;
+import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+
+/**
+ * Allows for elements to have an auto expand by adding a class
+ *
+ * @author Marc Magon
+ * @since 16 Jun 2017
+ */
+@PluginInformation(pluginName = "Angular Ion Slider",
+        pluginDescription = "Watches Angular Values for Ion Slider",
+        pluginUniqueName = "jwebswing-angular-auto-focus",
+        pluginVersion = "1.0.0",
+        pluginCategories = "angular,focus,ui,web ui, framework",
+        pluginSubtitle = "Watches Angular Values for Ion Slider",
+        pluginSourceUrl = "https://www.akveo.com/products.html",
+        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-Angular-Ion-Slider/wiki",
+        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-Angular-Ion-Slider",
+        pluginIconUrl = "",
+        pluginIconImageUrl = "",
+        pluginOriginalHomepage = "https://www.akveo.com/products.html",
+        pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularIonSlider.jar/download"
+)
+@ComponentInformation(name = "Angular Ion Slider",
+        description = "Watches Angular Values for Ion Slider",
+        url = "https://www.akveo.com/products.html")
+public class AngularIonSliderPageConfigurator extends PageConfigurator
+{
+
+    private static final long serialVersionUID = 1L;
+
+    /*
+     * Constructs a new AngularIonSliderPageConfigurator
+     */
+    public AngularIonSliderPageConfigurator()
+    {
+        //Nothing needed
+    }
+
+    @Override
+    public Page configure(Page page)
+    {
+        if (!page.isConfigured())
+        {
+            JQueryPageConfigurator.setRequired(page.getBody(), true);
+            AngularPageConfigurator.setRequired(page.getBody(), true);
+            page.getAngular().getAngularDirectives().add(new AngularIonSliderDirective());
+        }
+        return page;
+    }
+}
---
+++ test/za/co/mmagon/jwebswing/plugins/angularionslider/AngularIonSliderPageConfiguratorTest.java
@@ -0,0 +1,25 @@
+package za.co.mmagon.jwebswing.plugins.angularionslider;
+
+import org.junit.Test;
+import za.co.mmagon.BaseTestClass;
+import za.co.mmagon.jwebswing.Page;
+
+/**
+ *
+ * @author Marc Magon
+ */
+public class AngularIonSliderPageConfiguratorTest extends BaseTestClass
+{
+
+    public AngularIonSliderPageConfiguratorTest()
+    {
+    }
+
+    @Test
+    public void testSomeMethod()
+    {
+        Page p = getInstance();
+        System.out.println(p.toString(0));
+    }
+
+}
