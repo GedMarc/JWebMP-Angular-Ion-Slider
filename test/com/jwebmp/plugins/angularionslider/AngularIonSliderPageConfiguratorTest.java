52a729eef4699161fd91ff5de7b6fc5a7ed501b6
GedMarc <ged_marc@hotmail.com>
2018-04-22 15:47:27 +0200

    Prepping for RC1 - Update Base Package Name to new Name

--- test/com/jwebmp/plugins/angularionslider/AngularIonSliderPageConfiguratorTest.java
+++ test/com/jwebmp/plugins/angularionslider/AngularIonSliderPageConfiguratorTest.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.angularionslider;
 
-import com.jwebmp.BaseTestClass;
 import com.jwebmp.Page;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +7,7 @@ import org.junit.jupiter.api.Test;
  * @author Marc Magon
  */
 public class AngularIonSliderPageConfiguratorTest
-		extends BaseTestClass
+
 {
 
 	public AngularIonSliderPageConfiguratorTest()
@@ -18,7 +17,7 @@ public class AngularIonSliderPageConfiguratorTest
 	@Test
 	public void testSomeMethod()
 	{
-		Page p = getInstance();
+		Page p = new Page();
 		System.out.println(p.toString(0));
 	}
 
