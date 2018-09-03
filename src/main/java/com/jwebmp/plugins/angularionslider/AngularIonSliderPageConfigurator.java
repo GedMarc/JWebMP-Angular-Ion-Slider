f2043fa3b9b237c69a2dabba56af9e11cb93a253
GedMarc <ged_marc@hotmail.com>
2018-09-03 22:50:52 +0200

    Make page configuration optional, update the app

--- src/main/java/com/jwebmp/plugins/angularionslider/AngularIonSliderPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularionslider/AngularIonSliderPageConfigurator.java
@@ -51,12 +51,42 @@ import javax.validation.constraints.NotNull;
 public class AngularIonSliderPageConfigurator
 		implements IPageConfigurator
 {
+	/**
+	 * If this configurator is enabled
+	 */
+	private static boolean enabled = true;
+
 	/*
 	 * Constructs a new AngularIonSliderPageConfigurator
 	 */
 	public AngularIonSliderPageConfigurator()
 	{
 		//Nothing needed
+	}
+
+	/**
+	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static boolean isEnabled()
+	{
+		return AngularIonSliderPageConfigurator.enabled;
+	}
+
+	/**
+	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @param mustEnable
+	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static void setEnabled(boolean mustEnable)
+	{
+		AngularIonSliderPageConfigurator.enabled = mustEnable;
 	}
 
 	@NotNull
@@ -69,5 +99,11 @@ public class AngularIonSliderPageConfigurator
 			AngularPageConfigurator.setRequired(true);
 		}
 		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return AngularIonSliderPageConfigurator.enabled;
 	}
 }
