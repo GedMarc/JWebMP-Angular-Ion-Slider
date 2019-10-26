fe0233cbb1382275a05ad73fccad68c7cce17610
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:34:59 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -6,13 +6,13 @@ module com.jwebmp.plugins.angularionslider {
 	requires com.jwebmp.plugins.ionrangeslider;
 
 	requires com.google.common;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularionslider.AngularIonSliderPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularDirective with com.jwebmp.plugins.angularionslider.AngularIonSliderDirective;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularionslider.implementations.AngularIonSliderExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularionslider.implementations.AngularIonSliderExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularionslider.implementations.AngularIonSliderExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularionslider.implementations.AngularIonSliderExclusionsModule;
 
 }
